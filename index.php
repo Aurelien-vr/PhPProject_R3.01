38affc27c6595a0c8e5c13c9aeb9a380bb0d3f97
Aurios6 <80596073+Aurios6@users.noreply.github.com>
2024-11-18 07:15:17 +0100

    base form
    to continue

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>hello</p>
-</body>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+    <div id="container">
+        <form action="verification.php" method="POST">
+        <h1>Login</h1>
+        
+        <input type="text" placeholder="username" name="username" required>
+        <input type="password" placeholder="password" name="password" required>
+
+        <input type="submit" id='submit' value='LOGIN' >
+        </form>
+    </div>
+    </body>
 </html>
