e5f4e0dbdd1bcb7651802c1680e106864ff48064
Le Velly Malek <maleklevelly@gmail.com>
2024-11-28 09:37:13 +0100

    rien

---
+++ acceuil.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <title>Volley Manager</title>
+    </head>
+    <body>
+        <?php
+            header('Location: //index.php');
+            
+        ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <html>
+   
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
         <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <title>Volley Manager</title>
     </head>
     <body>
+    <?php
+        // http://localhost/PhPProject_R3.01/index.php
+
+        // Connection à la bd
+        try {
+            $linkpdo = new PDO("mysql:host=localhost;dbname=carnetcontact", "root", "");
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        if(false){
+            header('Location: /PhPProject_R3.01/acceuil.php');
+            exit();
+        }
+    ?>
     <div class="loginPannel" >
         <h1 id="loginTitle">LOGIN</h1>
         <form action="verification.php" method="POST">
