20d31c6da38dcd9efd7ccee59c8516ceb6a9544c
Aurélien <aurelvr@gmail.com>
2025-01-12 10:21:25 +0100

    styling and button to add player

--- php/joueurs.php
+++ php/joueurs.php
@@ -24,6 +24,7 @@ echo "<script>console.log('$joueursJson');</script>";
             <tr>
                 <th>ID</th>
                 <th>Name</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
@@ -41,13 +42,14 @@ echo "<script>console.log('$joueursJson');</script>";
                     echo "<tr class='collapsible' onclick='toggleRow(this)'>";
                     echo "<td>{$id}</td>";
                     echo "<td>{$name}</td>";
+                    echo "<td><form method='POST' action='modifier_joueur.php'><button type='submit' class='editPlayerButton' name='player_id' value='{$id}'>Edit player</button></form></td>";
                     echo "</tr>";
                     echo "<tr class='hidden hiddenStill'>";
-                    echo "<td colspan='2'>{$details}</td>";
+                    echo "<td colspan='3'>{$details}</td>";
                     echo "</tr>";
                 }
             } else {
-                echo "<tr><td colspan='2'>No players found.</td></tr>";
+                echo "<tr><td colspan='3'>No players found.</td></tr>";
             }
             ?>
         </tbody>
