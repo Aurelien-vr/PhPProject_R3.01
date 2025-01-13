60983fc8bcc34efa4139d667166366e2067c2910
Le Velly Malek <maleklevelly@gmail.com>
2025-01-13 16:48:48 +0100

    bouton ajouter jouer

--- php/joueurs.php
+++ php/joueurs.php
@@ -56,6 +56,9 @@
     </table>
 </div>
 
+<button onclick="window.location.href = 'ajout_joueurs.php';">AJOUTER JOUEUR</button>
+
+
 <script>
     function toggleRow(row) {
         const nextRow = row.nextElementSibling;
