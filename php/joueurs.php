558879bfe4afa114987fc09cf3d0ff28c4217b19
Le Velly Malek <maleklevelly@gmail.com>
2025-01-13 16:26:09 +0100

    rdfgc

--- php/joueurs.php
+++ php/joueurs.php
@@ -30,37 +30,43 @@
         }
     ?>
 
-        <div id="containerTable">
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr cclass="collapsible" onclick="toggleRow(this)">
-                        <td>1</td>
-                        <td>John Smith</td>
-                    </tr>
-                    <tr class="hidden hiddenStill">
-                        <td colspan="2">More details about John Smith...</td>
-                    </tr>
-                    <tr class="foldable" onclick="toggleRow(this)">
-                        <td>2</td>
-                        <td>Alice Brown</td>
-                    </tr>
-                    <tr class="hidden hiddenStill">
-                        <td colspan="2">
-                            <div>
-                                <p>More details about Alice Brown...</p>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <button onclick="window.location.href = '\ajout_joueurs.php';">INSERER JOUEUR</button>
+<div id="containerTable">
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if (is_array($joueurs) && !empty($joueurs)) {
+                foreach ($joueurs as $joueur) {
+                    $id = htmlspecialchars($joueur['numLicence']);
+                    $name = htmlspecialchars($joueur['nom'] . ' ' . $joueur['prenom']);
+                    $details = 'Date de naissance: ' . htmlspecialchars($joueur['dateNaissance']) .
+                    '<br>Taille: ' . htmlspecialchars($joueur['taille']) .
+                    '<br>Poids: ' . htmlspecialchars($joueur['poids']) .
+                    '<br>Statut: ' . htmlspecialchars($joueur['statutJoueur']) .
+                    '<br>Commentaire: ' . htmlspecialchars($joueur['commentaire']);
+
+                    echo "<tr class='collapsible' onclick='toggleRow(this)'>";
+                    echo "<td>{$id}</td>";
+                    echo "<td>{$name}</td>";
+                    echo "<td><form method='GET' action='ajout_joueurs.php'><button type='submit' class='editButton' name='numLicence' value='{$id}'>Edit player</button></form></td>";
+                    echo "</tr>";
+                    echo "<tr class='hidden hiddenStill'>";
+                    echo "<td colspan='3'>{$details}</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='3'>No players found.</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
 
     <script>
         function toggleRow(row) {
